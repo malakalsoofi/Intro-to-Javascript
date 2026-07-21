a6ebf20f26609d86cc5602865653bc36431f867f
malakalsoofi <malakalsoofi63@gmail.com>
2026-07-21 14:44:50 +0300

    message
    git commit -m "message"
    git push origin main
    "

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log('works!')
+let favSnack;
+// let is the declaration
+// favSnack is the identifier
+console.log(favSnack)
+
+
+let name = 'Tony Stark';
+console.log(name)
+
+name = 'Pepper Potts';
+// note that we only declare a variable once
+// const username ='malak';
+// console.log(username)
+let myString = 'Hello World';
+let myOtherString = "Hello World again";
+console.log(typeof myString);
+// Prints: string
+let myNumber = 15;
+console.log(typeof myNumber);
+// Prints: number
+console.log(typeof 12.34);
+let cohort;
+console.log(cohort);
+// Prints: undefined
+console.log(typeof null);
+// Prints: object 
+let username = 'friend';
+let greeting = `Hello,`;
+// let greeting = `Hello, ${username}.`;
+console.log(greeting);
+// Prints: Hello, friend.
+let longString = `Hello, and welcome to the application!
+Get started by logging in below!
+We're happy to have you.`;
+let c='1'+1
+console.let(typeof c);
+'1'===1
